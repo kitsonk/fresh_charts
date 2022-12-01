a321e48580e05ee46640a807cc58cddf2ba3fb87
Kitson Kelly <me@kitsonkelly.com>
2022-12-01 14:52:28 +1100

    feat: expose ChartJS global defaults

--- mod.ts
+++ mod.ts
@@ -51,5 +51,10 @@
  */
 
 export { Chart } from "./Chart.tsx";
-export { type ChartConfiguration, type ChartOptions, plugins } from "./core.ts";
+export {
+  type ChartConfiguration,
+  type ChartOptions,
+  globalDefaults,
+  plugins,
+} from "./core.ts";
 export { renderChart } from "./render.ts";
